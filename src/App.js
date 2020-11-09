d9ae2e67f0303137a086cc2e999de424efcb96f6
William <William.vage@gmail.com>
2020-11-09 10:27:18 +0100

    Added react Bootstrap and Firebase

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { Component } from 'react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <h1>
-          Welcome to Visualizar! 
-           
-        </h1>
-        <p>
-          There wont be any photosharing here
-        </p>
+          Welcome to Visualizar!
+        </h1>        
+
+        <Button>
+          <p>
+            There wont be any photosharing here
+        </p></Button>
         <a
           className="App-link"
           href="https://reactjs.org"
