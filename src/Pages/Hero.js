9ada96da233a90b44bc30639e24c0603b90d091d
William <William.vage@gmail.com>
2020-11-10 14:31:04 +0100

    Completed uploading image task ( will need to make better )

--- src/Pages/Hero.js
+++ src/Pages/Hero.js
@@ -11,10 +11,12 @@ const Hero = ({ handleLogout }) => {
         {/* <div className="logo-page-contain">
           <img className="logo-page" src={logo} alt={"logo"} /> 
         </div> */}
+        
         <button onClick={handleLogout}>Logout</button>   
         {/* BURGER MENU GOES HERE> */}
        
       </nav>
+      <Upload/>
     </section>
   );
 };
